712ba68e6887a3c0b8b28c10dcbd109e486c6379
Umamad <marandmohammad@gmail.com>
2021-10-04 20:12:54 +0330

    PortfolioSection, TestimonialsSection & FactsSection left

--- src/components/sections/services.component.jsx
+++ src/components/sections/services.component.jsx
@@ -6,39 +6,39 @@ const ServicesSection = ({ lang }) => (
 
         <div class="section-title">
           <h2>سرویس ها</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <p>برای اطلاع از نحوه ارائه خدمات، گستره سرویس ها و مشاوره در مورد دیجیتال کردن کسب و کار خود تماس بگیرید.</p>
         </div>
 
         <div class="row">
           <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
             <div class="icon"><i class="bi bi-columns-gap"></i></div>
             <h4 class="title"><a href="">طراحی رابط کاربری</a></h4>
-            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+            <p class="description">اولین قضاوت بر اساس ظاهر انجام می شود پس ظاهر زیبا در وب سایت و اپ شما از اهمیت بالایی برخوردار است.</p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
             <div class="icon"><i class="bi bi-diagram-3"></i></div>
             <h4 class="title"><a href="">طراحی دیتابیس</a></h4>
-            <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+            <p class="description">نحوه ذخیره اطلاعات و روابط میان آن ها باید به سادگی انجام شود تا از پیچیدگی برنامه کاسته شود.</p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
             <div class="icon"><i class="bi bi-globe2"></i></div>
             <h4 class="title"><a href="">وب سایت تبلیغاتی</a></h4>
-            <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            <p class="description">وبسایت های تبلیغاتی بهترین بستر برای معرفی شما به دیگران به صورت صوری و متنی است.</p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
             <div class="icon"><i class="bi bi-code-slash"></i></div>
             <h4 class="title"><a href="">گسترش صفحات وب</a></h4>
-            <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <p class="description">در عصر دیجیتال همیشه باید بروز بود، آپدیت و تغییرات باید کاملا زیبا و کاربر پسند باشد.</p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
             <div class="icon"><i class="bi bi-bug"></i></div>
             <h4 class="title"><a href="">اشکال زدایی ( دیباگ )</a></h4>
-            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+            <p class="description">ایرادات توجهات منفی را به کسب و کار شما جلب می کنند، رفع ایرادات بسیار ضروری و مهم است.</p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
             <div class="icon"><i class="bi bi-code-square"></i></div>
             <h4 class="title"><a href="">اپ اندروید و دسکتاپ</a></h4>
-            <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+            <p class="description">ساخت برنامه های اندروید و دسکتاپ برای کسب و کار شما با زیبایی و کارایی بالا</p>
           </div>
         </div>
       </div>
@@ -47,39 +47,39 @@ const ServicesSection = ({ lang }) => (
 
           <div class="section-title">
             <h2>Services</h2>
-            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+            <p>Call for information on how to provide services, scope of services and advice on Digitize your business.</p>
           </div>
 
           <div class="row">
             <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
               <div class="icon"><i class="bi bi-columns-gap"></i></div>
               <h4 class="title"><a href="">UI Design</a></h4>
-              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+              <p class="description">The first judgment is based on appearance, so a beautiful appearance on your website and app is very important.</p>
             </div>
             <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
               <div class="icon"><i class="bi bi-diagram-3"></i></div>
               <h4 class="title"><a href="">DataBase Design</a></h4>
-              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+              <p class="description">How to store information and the relationships between them should be done simply to reduce the complexity of the program.</p>
             </div>
             <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
               <div class="icon"><i class="bi bi-globe2"></i></div>
               <h4 class="title"><a href="">Advertising WebSite</a></h4>
-              <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+              <p class="description">Advertising websites are the best platform for introducing you to others both media and textually ways.</p>
             </div>
             <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
               <div class="icon"><i class="bi bi-code-slash"></i></div>
               <h4 class="title"><a href="">Web Development</a></h4>
-              <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+              <p class="description">In the digital age, must always be up to date, updates and changes must be quite beautiful and user-friendly.</p>
             </div>
             <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
               <div class="icon"><i class="bi bi-bug"></i></div>
               <h4 class="title"><a href="">Debug</a></h4>
-              <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+              <p class="description">Defects draw negative attention to your business, it is very necessary and important to fix the defects.</p>
             </div>
             <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
               <div class="icon"><i class="bi bi-code-square"></i></div>
               <h4 class="title"><a href="">Android &amp; Desktop App</a></h4>
-              <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+              <p class="description">Build Android and desktop applications for your business with beauty and high performance</p>
             </div>
           </div>
         </div>
